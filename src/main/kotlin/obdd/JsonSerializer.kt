de3eb27a78911fd8183afc47c845ccf0e2d079d5
rec0de <mail@rec0de.net>
2021-05-30 13:02:08 +0200

    Add reduce, json and outfile options

---
+++ src/main/kotlin/obdd/JsonSerializer.kt
@@ -0,0 +1,35 @@
+package obdd
+
+import de.tu_darmstadt.rs.logictool.bdd.representation.Bdd
+import java.io.BufferedWriter
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
+
+object JsonSerializer {
+    fun writeToFile(bdd: Bdd, filename: String) {
+        val indexMap = bdd.nodes.withIndex().associate { Pair(it.value, it.index) }
+
+        BufferedWriter(OutputStreamWriter(FileOutputStream(filename))).use { out ->
+            out.write("{\n")
+            out.write("\"root\": ${indexMap[bdd.rootNode]},\n")
+            out.write("\"zeroNode\": ${indexMap[bdd.zeroNode]},\n")
+            out.write("\"oneNode\": ${indexMap[bdd.oneNode]},\n")
+            out.write("\"nodes\":\n\t[\n")
+
+            val nodeBlock = bdd.nodes.joinToString(",\n") {
+                val label = when (it) {
+                    bdd.zeroNode -> "0"
+                    bdd.oneNode -> "1"
+                    else -> it.variable.name
+                }
+                val zero = indexMap[it.zeroChild]
+                val one = indexMap[it.oneChild]
+                "\t\t{\"variable\": \"$label\", \"zeroChild\": $zero, \"oneChild\": $one}"
+            }
+
+            out.write(nodeBlock)
+            out.write("\n\t]\n")
+            out.write("}")
+        }
+    }
+}
--- src/main/kotlin/obdd/main.kt
+++ src/main/kotlin/obdd/main.kt
@@ -1,23 +1,29 @@
 package obdd
 
 import de.tu_darmstadt.rs.logictool.bdd.tools.BddFactory
+import de.tu_darmstadt.rs.logictool.bdd.tools.BddReducer
 
 fun main(args: Array<String>) {
     val flags = args.filter { it.startsWith("--") }
     val other = args.filter { !it.startsWith("--") }
 
+    // Print help if no (or more than one) formula was given
     if(other.size != 1) {
         println("Usage: obdd-gen [flags] [formula]")
         println("--naive\tUse naive function evaluation (rather than syntactic simplification)")
+        println("--reduce\tProduce a fully reduced bdd")
+        println("--json\tOutput a json representation rather than a dot graph")
         println("--order=[none|weight|a,b,c]\tSpecify variable evaluation order (default: weight)")
+        println("--out=[path]\tWrite output to this location (default: bdd.dot / bdd.json)")
         return
     }
 
+    // Parse formula
     val formula = FormulaConverter.parse(other.first())
     println("Parsed input formula: $formula")
 
+    // Compute order according to flag / verify given order contains all required vars
     val varWeights = formula.computeVarWeights(Int.MAX_VALUE)
-
     val order = when(val orderFlag = flags.firstOrNull{ it.startsWith("--order=") }) {
         "--order=none" -> varWeights.keys.toList()
         "--order=weight", null -> varWeights.toList().sortedByDescending { it.second }.map { it.first }
@@ -28,16 +34,31 @@ fun main(args: Array<String>) {
             varList
         }
     }
-
     println("Using variable order: '${order.joinToString(", ")}'")
 
     val naiveEval = flags.contains("--naive")
+    val reduce = flags.contains("--reduce")
+    val jsonOut = flags.contains("--json")
+    val filename = when(val outFlag = flags.firstOrNull{ it.startsWith("--out=") }) {
+        null -> if(jsonOut) "bdd.json" else "bdd.dot"
+        else -> outFlag.removePrefix("--out=")
+    }
 
+    // Create bdd from formula
     val bdd = if(naiveEval)
             BddFactory().create(FormulaBasedFunction(formula), genOrder(order))
         else
             BddBuilder.create(formula, order)
 
-    //println(bdd) // TODO: produce usable representation
-    DotSerializer.writeToFile(bdd, "g.dot")
+    // Reduce, if necessary
+    if(reduce)
+        BddReducer().reduceBdd(bdd)
+
+    // Write result to file
+    if(jsonOut)
+        JsonSerializer.writeToFile(bdd, filename)
+    else
+        DotSerializer.writeToFile(bdd, filename)
+
+    println("Wrote generated BDD to file '$filename'")
 }
